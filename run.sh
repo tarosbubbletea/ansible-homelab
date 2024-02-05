fd2a6af6a75766ff4ace6aa965bf2a8a69d2b3bf
tarosbubbletea <avgtafull@gmail.com>
2024-02-05 16:21:46 -0300

    fixed nearly all the basic stuff

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-ansible-playbook -k --vault-password-file=ansible_pwd site.yml #run without -k past first run
+
+#must run the first line after installing distro to set up ansible support on the host
+#my suggestion? modify a debian image to configure unattended install and have the ansible user be created automatically
+#otherwise, you will have to enable login as root on the ssh config file and run the first playbook 
+
+# ansible-playbook -k --vault-password-file=ansible_pwd -v setup_ansible_user.yml
+ansible-playbook --vault-password-file=ansible_pwd -v site.yml
 #ansible-playbook --vault-password-file=ansible_pwd -t checkup site.yml
