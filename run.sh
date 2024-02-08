0d4c34e4deae35dca7ffe4643a2cf4ebf857c8f9
tarosbubbletea <avgtafull@gmail.com>
2024-02-08 18:11:30 -0300

    grafana, etc.

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@
 #otherwise, you will have to enable login as root on the ssh config file and run the first playbook 
 
 # ansible-playbook -k --vault-password-file=ansible_pwd -v setup_ansible_user.yml
-ansible-playbook --vault-password-file=ansible_pwd -v site.yml
+ansible-playbook --vault-password-file=ansible_pwd -v site.yml $@
 #ansible-playbook --vault-password-file=ansible_pwd -t checkup site.yml
