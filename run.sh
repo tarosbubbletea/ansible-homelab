966a8a847d3d55d75cf27c57973b65c7b0837303
tarosbubbletea <avgtafull@gmail.com>
2024-02-09 20:07:35 -0300

    grafana refactor, homepage

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 #must run the first line after installing distro to set up ansible support on the host
 #my suggestion? modify a debian image to configure unattended install and have the ansible user be created automatically
-#otherwise, you will have to enable login as root on the ssh config file and run the first playbook 
+#otherwise, you will have to enable login as root on the ssh config file and run the first playbook
 
 # ansible-playbook -k --vault-password-file=ansible_pwd -v setup_ansible_user.yml
-ansible-playbook --vault-password-file=ansible_pwd -v site.yml $@
+ansible-playbook --vault-password-file=ansible_pwd site.yml $@
 #ansible-playbook --vault-password-file=ansible_pwd -t checkup site.yml
